22b15df84e2f22035bfa3da452b58631b0fc5cc1
Alejandro Garrido <alejandro.garrido@vipera.com>
2020-09-15 14:30:07 +0200

    100% test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import catalogo from './Utils/catalogo';
 function App() {
   return (
     <div className="App">
+      <div>Lista de la compra</div>
       <ListaDeLaCompra catalogo={catalogo}></ListaDeLaCompra>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Lista de la compra/i);
+  expect(linkElement).toBeInTheDocument();
 });
