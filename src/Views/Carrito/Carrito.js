281b399136753d43e80819eb7fca1be10ad903cd
Alejandro Garrido <alejandro.garrido@vipera.com>
2020-09-17 12:16:09 +0200

    100% 5/5 3/3 carrito done

--- src/Views/Carrito/Carrito.js
+++ src/Views/Carrito/Carrito.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 
-const Carrito = ({listaDeLaCompra}) => {
+const Carrito = ({ listaDeLaCompra }) => {
     return (
         <div>
-            
+            <Accordion>
+                <AccordionSummary>
+                    <ShoppingCartIcon fontSize='large' />
+                </AccordionSummary>
+                <AccordionDetails>
+                    {'cosas que vas a comprar'}
+                </AccordionDetails>
+            </Accordion>
         </div>
     )
 }
